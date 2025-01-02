6f429077bf4d35cabb115c1f386b0ee45ef2d0db
knh27 <knhxptn@gmail.com>
2025-01-02 23:38:14 +0530

    finding element that is appearing only one time in array using HashMap

--- src/main/java/com/pranav/striver_dsa_sheet/arrayeasy/AppearOneTime.java
+++ src/main/java/com/pranav/striver_dsa_sheet/arrayeasy/AppearOneTime.java
@@ -1,12 +1,17 @@
 package com.pranav.striver_dsa_sheet.arrayeasy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AppearOneTime {
     public static void main(String[] args) {
         AppearOneTime obj=new AppearOneTime();
-//        int[] a={6,1,4,2,1,5,2,6,4,1};
+        int[] a={6,1,4,2,1,5,2,6,4,1};
 //        int[] a={4,1,2,1,2};
-        int[] a={2,2,1};
-        obj.AppearOneTime_bruteForce(a);
+//        int[] a={2,2,1};
+//        obj.AppearOneTime_bruteForce(a);
+        obj.AppearOneTime_betterApproach(a);
+
     }
 
     void AppearOneTime_bruteForce(int[] a){
@@ -32,4 +37,18 @@ public class AppearOneTime {
         }
     }
 
+    void AppearOneTime_betterApproach(int[] a){
+        Map<Integer, Integer> myMap=new HashMap<>();
+        for(Integer i:a){
+            myMap.put(i, myMap.getOrDefault(i,0)+1);
+        }
+//        for(Map.Entry<Integer, Integer> i:myMap.entrySet()){
+//            System.out.println(i);
+//        }
+        for(Map.Entry<Integer, Integer> i:myMap.entrySet()){
+            if(i.getValue()==1){
+                System.out.println(i.getKey());
+            }
+        }
+    }
 }
