ebda4113a296522789b362b13db8b04e29129a4f
knh27 <knhxptn@gmail.com>
2025-01-03 23:32:11 +0530

    longest subarray with elements and sum positive- extreme brute force with time complexity o(n^3)

--- src/main/java/com/pranav/striver_dsa_sheet/arrayeasy/LongestSubArrayPosSum.java
+++ src/main/java/com/pranav/striver_dsa_sheet/arrayeasy/LongestSubArrayPosSum.java
@@ -2,10 +2,11 @@ package com.pranav.striver_dsa_sheet.arrayeasy;
 
 public class LongestSubArrayPosSum {
     public static void main(String[] args) {
-        int[]a={2,3,5,1,9};
+        int[]a={9,1,2,3,5,1,9};
         int sum=10, result=3;
         LongestSubArrayPosSum obj=new LongestSubArrayPosSum();
-        obj.longestSubArray1(a);
+//        obj.longestSubArray1(a);
+        obj.longestSubArray2(a,sum);
     }
 
 
@@ -21,6 +22,22 @@ public class LongestSubArrayPosSum {
                     if(max<(j-i+1)) max=j-i+1;
                 }
             }
+        }
+        System.out.println(max);
+    }
+
+    void longestSubArray2(int[] a, int k){
+        int max=0;int j;
+        for(int i=0;i<a.length;i++){
+            int sum=0;
+            for(j=i;j<a.length;j++){
+                sum=sum+a[j];
+                if(sum==k){
+                    max=Math.max(max,j-i+1);
+                }
+            }
+
+
         }
         System.out.println(max);
     }
