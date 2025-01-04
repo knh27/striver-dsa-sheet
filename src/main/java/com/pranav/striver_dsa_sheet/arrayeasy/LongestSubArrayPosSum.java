a60ec0ab5afbdb2fb89a7fa542c06f996dfe0f6a
knh27 <knhxptn@gmail.com>
2025-01-04 10:54:47 +0530

    longest subarray with elements and sum positive- using map

--- src/main/java/com/pranav/striver_dsa_sheet/arrayeasy/LongestSubArrayPosSum.java
+++ src/main/java/com/pranav/striver_dsa_sheet/arrayeasy/LongestSubArrayPosSum.java
@@ -1,12 +1,18 @@
 package com.pranav.striver_dsa_sheet.arrayeasy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubArrayPosSum {
     public static void main(String[] args) {
         int[]a={9,1,2,3,5,1,9};
         int sum=10, result=3;
         LongestSubArrayPosSum obj=new LongestSubArrayPosSum();
 //        obj.longestSubArray1(a);
-        obj.longestSubArray2(a,sum);
+
+//        obj.longestSubArray2(a,sum);
+
+        obj.longestSubArray_map(a, sum);
     }
 
 
@@ -41,6 +47,28 @@ public class LongestSubArrayPosSum {
         }
         System.out.println(max);
     }
+
+    void longestSubArray_map(int[] a, int k){
+        HashMap<Integer, Integer> preSum=new HashMap<>();
+        int sum=0, maxLen=0;
+        for(int i=0;i<a.length;i++){
+            sum+=a[i];
+            if(sum==k){
+                maxLen=Math.max(maxLen, i+1);
+            }
+            int rem=sum-k;
+
+            if(preSum.containsKey(rem)){
+                int len=i-preSum.get(rem);
+                maxLen=Math.max(maxLen, len);
+            }
+
+            if(!preSum.containsKey(sum)){
+                preSum.put(sum, i);
+            }
+
+        }
+        System.out.println(maxLen);
+    }
 }
 
-//
