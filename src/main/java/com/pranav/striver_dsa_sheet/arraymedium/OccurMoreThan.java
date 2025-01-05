23e9ab3ecf7d0e7dc2b2ebcb7570bdef79670e2e
knh27 <knhxptn@gmail.com>
2025-01-05 23:37:26 +0530

    element that occure more than N/2 where N is size of array: moore-voting-algo

--- src/main/java/com/pranav/striver_dsa_sheet/arraymedium/OccurMoreThan.java
+++ src/main/java/com/pranav/striver_dsa_sheet/arraymedium/OccurMoreThan.java
@@ -1,11 +1,17 @@
 package com.pranav.striver_dsa_sheet.arraymedium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OccurMoreThan {
     public static void main(String[] args) {
         OccurMoreThan obj=new OccurMoreThan();
-//        int[] num={3,2,3};
-        int[] num={2,2,1,1,1,2,2};
-        obj.occur_bruteforce(num);
+        int[] num={3,2,3};
+//        int[] num={2,2,1,1,1,2,2};
+//        obj.occur_bruteforce(num);
+//        obj.occur_map(num);
+
+        obj.moore_voting_algo(num);
     }
 
     void occur_bruteforce(int[] a){
@@ -27,4 +33,42 @@ public class OccurMoreThan {
             }
         }
     }
+
+
+    void occur_map(int[] a){
+        HashMap<Integer, Integer> freqMap=new HashMap<>();
+        for(Integer i:a){
+            int found=freqMap.getOrDefault(i,0);
+            freqMap.put(i, found+1);
+        }
+
+
+        for(Map.Entry<Integer, Integer> kv: freqMap.entrySet()){
+            if(kv.getValue()>a.length/2){
+                System.out.println(kv.getKey());
+                return;
+            }
+        }
+    }
+
+    void moore_voting_algo(int[] a){
+        int c=0;
+        int el=a[0];
+        for(int i=0;i<a.length;i++){
+            if(c==0){
+                c=1;
+                el=a[i];
+            }
+            else if(el==a[i])c++;
+            else c--;
+        }
+        int cnt=0;
+        for(int i=0;i<a.length;i++){
+            if(el==a[i]) cnt++;
+        }
+        if(cnt>a.length/2) System.out.println(el);
+        else System.out.println("no such element");
+    }
+
+
 }
