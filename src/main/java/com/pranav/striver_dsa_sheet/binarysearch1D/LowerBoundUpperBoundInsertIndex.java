ec87a30811ea3ffb0ea6aceb9b307b93fe641ae0
knh27 <knhxptn@gmail.com>
2025-02-09 23:01:27 +0530

    insert index/ lower bound and upper bound- optimal approach using binary search with time complexity o(logn) of base 2

---
+++ src/main/java/com/pranav/striver_dsa_sheet/binarysearch1D/LowerBoundUpperBoundInsertIndex.java
@@ -0,0 +1,36 @@
+package com.pranav.striver_dsa_sheet.binarysearch1D;
+
+public class LowerBoundUpperBound {
+    public static void main(String[] args) {
+        int[] a={1,3,5,7,9,11,14,16,19};
+        int tar=17;
+        int ans=ub(a,19);
+        System.out.println(ans);
+    }
+
+    static int lb(int[] a, int tar){ //index such that element at that index greater than or equal to tar
+        int l=0, h=a.length-1;
+        int ans=a.length;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(a[m]>=tar){
+                ans=m;
+                h=m-1;
+            }else l=m+1;
+        }
+        return ans;
+
+    }
+
+    static int ub(int[] a, int tar){ //index such that element at that index greater than target
+        int l=0, h=a.length-1,ans=a.length;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(a[m]>tar){
+                ans=m;
+                h=m-1;
+            }else l=m+1;
+        }
+        return ans;
+    }
+}
